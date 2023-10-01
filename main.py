7d7f738b8fa0519e291d4f01e73fcaf26d336e79
RobinPICODEV <0x.88ta.ko@gmail.com>
2023-10-01 16:35:21 +0200

    update : declare variable, loop for.

---
+++ main.py
@@ -0,0 +1,25 @@
+import string
+import getpass
+
+def check_password_strength():
+    password = getpass.getpass('Enter your password:')
+    strength = 0
+    remarks = ''
+    lower_count = upper_count = num_count = wspace_count = special_count = 0
+
+    for char in list(password):
+        if char in string.ascii_lowercase:
+            lower_count += 1
+        elif char in string.ascii_uppercase:
+            upper_count += 1
+        elif char in string.digits:
+            num_count += 1
+        elif char == ' ':
+            wspace_count += 1
+        else:
+            special_count += 1
+
+
+
+if __name__ == '__main__':
+    print('Welcome to Password Strength Checker')
